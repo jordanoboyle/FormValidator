bddb6a23a449cfc54219d56f3558916ee10a8438
jordanoboyle <jmcmcode@gmail.com>
2024-07-31 22:34:29 -0600

    email validation using regex, else if statement rendering different message for invalid

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ const password2 = document.getElementById('password2');
 function showFailure(input, message) {
   const formControl = input.parentElement;  //refer to index.html for info as to what we are grabbing here
   formControl.className = 'form-control failure';
+  const small = formControl.querySelector('small'); //querySelector can take in a class, tag, id
+  small.innerText = message; 
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success'; //dictated from our css colors
+}
+
+//Validate Email
+function isEmailValid(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLocaleLowerCase()); //use the .test() method here to see if the email String matches the the REGEX. If the match is successful, a boolean will be returned (true or false)
 }
 
 //we need an event listener here for our form:
@@ -23,6 +36,24 @@ form.addEventListener('submit', function(e) {
     showFailure(username, "Username is required");
   } else {
     showSuccess(username);
+  }
+  if (email.value === '') {
+    // alert("email required");
+    showFailure(email, "Email is required");
+  } else if (!isEmailValid(email.value)) {
+    showFailure(email, "Email is not valid.");
+  } else {
+    showSuccess(email);
+  }
+  if (password.value === '') {
+    showFailure(password, 'Password required');
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === '') {
+    showFailure(password2, 'Password2 required');
+  } else {
+    showSuccess(password2);
   }
 });
 

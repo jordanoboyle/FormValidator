09952d043e582b3f8d9e2fdb3335ed99e42948bf
jordanoboyle <jmcmcode@gmail.com>
2024-08-03 00:13:29 -0600

    change password2 type to password

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function showSuccess(input) {
   formControl.className = 'form-control success'; //dictated from our css colors
 }
 
-//Validate Email
+//Check Email validation
 function checkEmail(input) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value)) {
